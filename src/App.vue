8ac2c07434ce2e566634a9074aeef75493466575
lhmmhs <lhmmhsa@gmail.com>
2022-06-28 20:34:31 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import lButton from "@/components/Button.vue";
+</script>
+
+<template>
+  <l-button>卧槽</l-button>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineComponent } from 'vue';
+
+defineComponent({
+  name: 'LhButton',
+});
+
+console.log(1);
+</script>
+
+<template>
+  <button class="l-button">
+    <slot />
+  </button>
+</template>
+
+<style lang="less">
+.l-button {
+  background: #ccc;
+}
+</style>
