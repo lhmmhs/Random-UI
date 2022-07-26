4a449f135f940450e9d66ffc9c3fa606f5435d1a
lhmmhs <lhmmhsa@gmail.com>
2022-07-26 22:17:14 +0800

    feat: icon

---
+++ src/components/Icon/icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    :class="classes(BEM())"
+    :style="{
+      color: props.color,
+      fontSize: props.size,
+    }"
+    aria-hidden="true"
+  >
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { computed, defineComponent } from 'vue';
+import { createNamespace } from '@/utils';
+import './font/iconfont';
+import { iconProps } from './props';
+
+const { BEM, classes } = createNamespace('icon');
+
+defineComponent({ name: 'RandomIcon' });
+const props = defineProps(iconProps);
+
+const iconName = computed(() => `#random-${props.name}`);
+</script>
+
+<style lang="less">
+@import './index.less';
+</style>
---
+++ src/components/Icon/props.ts
@@ -0,0 +1,11 @@
+export const iconProps = {
+  name: {
+    type: String,
+  },
+  size: {
+    type: [Number, String],
+  },
+  color: {
+    type: String,
+  },
+};
